d3c55d01905b24737c64f296f8f79dea75f4b9ec
Otavio-Leonel <69874641+Otavio-Leonel@users.noreply.github.com>
2023-05-30 00:29:48 -0300

    Add files via upload

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -1,21 +1,35 @@
-{/*MICROSERVICO CRIADO A PARTE
-*/}
-{/*esses const's são para enviar os eventos para os microservicos*/}
-const express = require('express');
-const bodyParser = require('body-parser');
-{/*AXIOS É UMA BIBLIOTECA, PARA REQUISIÇÃO HTTP, SIMPLIFICA O PROCESSO DE SOLICITAR HTTP*/}
-const axios = require('axios');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql2')
+const app = express();
+app.use(express.json())
+const axios = require('axios')
 
-const app = express();{/*Aqui são as rotas de configuração */}
-app.use(bodyParser.json()); {/*analisa o corpo das solicitações HTTP*/}
+
+app.get('/cadastro', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'bd_ususarios',
+        password: '821160656'
+    })
+
+    connection.query('select * from tb_usuarios', (err, results, fields) => {
+        res.send (results)
+        res.send('ok')
+    })
+})
+app.use(bodyParser.json()); /*analisa o corpo das solicitações HTTP*/
+
 
 app.post('/eventos', (req, res) => {
     const evento = req.body;
-    axios.post('http://localhost:6000/eventos', evento); {/*Barramento do cadastro */}
-    axios.post('http://localhost:7000/eventos', evento); {/*Barramento localizacao */}
+    axios.post('http://localhost:3000/eventos', evento); /*Barramento do cadastro */
+   // axios.post('http://localhost:3000/eventos', evento); {/*Barramento localizacao 
+    axios.post('http://localhost:4000/eventos', evento); /*Envia o evento para o microservico de busca */
     res.status(200).send({msg: 'ok'});
 
 });
-app.listen(10000, () => {
-    console.log('Barramento de eventos de cadastro e localizacao. Posta 10000.')
+app.listen(5000, () => {
+    console.log('Barramento de eventos de cadastro e localizacao. Posta 5000.')
 })
